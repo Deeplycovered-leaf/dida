9e66efc4e6adc48fed561c5e66e674fc3468ceeb
d-leaf <346691836@qq.com>
2023-07-26 22:48:57 +0800

    feat: add smart-projects test

--- apps/teach-frontend/src/store/tests/smart-projects.spec.ts
+++ apps/teach-frontend/src/store/tests/smart-projects.spec.ts
@@ -1,7 +1,20 @@
-import { beforeEach, describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { createPinia, setActivePinia } from 'pinia'
-import { useTasksSelectorStore } from '../tasksSelector'
-import { SmartProjectName, completeSmartProject, trashSmartProject, useSmartProjects } from '../smartProjects'
+import { create_task_response } from './tasks.spec'
+import {
+  SmartProjectName,
+  TaskStatus,
+  completeSmartProject,
+  loadSmartProjectTasks,
+  trashSmartProject,
+  useSmartProjects,
+  useTasksSelectorStore,
+} from '@/store'
+import { fetchAllTasks } from '@/api'
+
+vi.mock('@/api')
+
+vi.mocked(fetchAllTasks).mockResolvedValue([])
 
 describe('smart projects store', () => {
   beforeEach(() => {
@@ -15,6 +28,7 @@ describe('smart projects store', () => {
     smart_project_store.selectProject(SmartProjectName.Complete)
 
     expect(tasks_selector_store.currentSelector).toEqual(completeSmartProject)
+    expect(fetchAllTasks).toBeCalledWith({ status: TaskStatus.COMPLETED, sortBy: 'updatedAt' })
   })
 
   it('should be select trash project when project name is trash', async () => {
@@ -24,5 +38,27 @@ describe('smart projects store', () => {
     smart_project_store.selectProject(SmartProjectName.Trash)
 
     expect(tasks_selector_store.currentSelector).toEqual(trashSmartProject)
+    expect(fetchAllTasks).toBeCalledWith({ status: TaskStatus.REMOVED, sortBy: 'updatedAt' })
+  })
+
+  it('should be load complete smart project tasks when SmartProjectName is complete', async () => {
+    vi.mocked(fetchAllTasks).mockResolvedValue([
+      create_task_response('task1', TaskStatus.COMPLETED),
+    ])
+    const tasks = await loadSmartProjectTasks(SmartProjectName.Complete)
+    expect(fetchAllTasks).toBeCalledWith({ status: TaskStatus.COMPLETED, sortBy: 'updatedAt' })
+    expect(tasks.length).toBe(1)
+    expect(tasks[0].status).toBe(TaskStatus.COMPLETED)
+  })
+
+  it('should be load removed smart project tasks when SmartProjectName is complete', async () => {
+    vi.mocked(fetchAllTasks).mockResolvedValue([
+      create_task_response('task1', TaskStatus.REMOVED),
+    ])
+    const tasks = await loadSmartProjectTasks(SmartProjectName.Trash)
+
+    expect(fetchAllTasks).toBeCalledWith({ status: TaskStatus.REMOVED, sortBy: 'updatedAt' })
+    expect(tasks.length).toBe(1)
+    expect(tasks[0].status).toBe(TaskStatus.REMOVED)
   })
 })
