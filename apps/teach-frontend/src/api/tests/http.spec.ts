2af073fea6ea92ac73f1aa60c08f366c6b718dcf
d-leaf <346691836@qq.com>
2023-07-28 22:32:05 +0800

    feat: add http test

--- apps/teach-frontend/src/api/tests/http.spec.ts
+++ apps/teach-frontend/src/api/tests/http.spec.ts
@@ -2,63 +2,58 @@ import { beforeEach, describe, expect, it, vi } from 'vitest'
 import AxiosMockAdapter from 'axios-mock-adapter'
 import { http } from '../http'
 import { cleanToken, setToken } from '@/utils/token'
-import { messageError, messageRedirectToSignIn } from '@/composables/message'
-import { goToLogin } from '@/composables'
+import { goToLogin, messageError, messageRedirectToSignIn } from '@/composables'
 
 vi.mock('@/composables/message')
 
 describe('http', () => {
-  let axiosMock: AxiosMockAdapter
+  let axios_mock: AxiosMockAdapter
   beforeEach(() => {
-    axiosMock = new AxiosMockAdapter(http)
+    axios_mock = new AxiosMockAdapter(http)
+
     cleanToken()
     vi.clearAllMocks()
   })
 
-  it('should set headers Authorization when have token', async () => {
+  it('should be set token in header when token exists', async () => {
     const token = 'token'
     setToken(token)
 
-    axiosMock.onGet('/tasks').reply(200, {
+    axios_mock.onGet('/tasks').reply(200, {
       code: 0,
       data: null,
       message: '',
     })
-
     await http.get('/tasks')
 
-    expect(axiosMock.history.get[0].headers?.Authorization).toBe(
-      `Bearer ${token}`,
-    )
+    expect(axios_mock.history.get[0].headers?.Authorization).toBe(`Bearer ${token}`)
   })
 
-  it('should return data of responseData when code is 0', async () => {
-    const data = 'cxr'
-    axiosMock.onGet('/tasks').reply(200, {
+  it('should return data of response when  code is 0', async () => {
+    const data = 'data'
+    axios_mock.onGet('/tasks').reply(200, {
       code: 0,
       data,
       message: '',
     })
-
     const result = await http.get('/tasks')
 
     expect(result).toBe(data)
   })
 
-  it('should throw an error and show error message when code is not 0', async () => {
-    const message = 'an error'
-    axiosMock.onGet('/tasks').reply(200, {
-      code: -1,
+  it('should throw error and show error message when code not is 0', async () => {
+    const message = 'error message'
+    axios_mock.onGet('/tasks').reply(200, {
+      code: 1,
       data: null,
       message,
     })
-
     await expect(() => http.get('/tasks')).rejects.toThrowError(message)
     expect(messageError).toBeCalledWith(message)
   })
 
-  it('should redirect to sign in and show error message when http status code is 401', async () => {
-    axiosMock.onGet('/tasks').reply(401)
+  it('should redirct to sign in and throw error when http status code is 401', async () => {
+    axios_mock.onGet('/tasks').reply(401)
 
     await expect(() => http.get('/tasks')).rejects.toThrowError()
     expect(messageRedirectToSignIn).toBeCalledWith(goToLogin)
